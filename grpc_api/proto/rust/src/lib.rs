c45e8612ce54e73ff4140b9c4109181a0092a30c
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2025-12-03 17:03:25 +0700

    Generate protobuf files for rust

---
+++ grpc_api/proto/rust/src/lib.rs
@@ -0,0 +1,17 @@
+pub mod auth {
+    pub mod api;
+    pub mod role;
+    pub mod user;
+    pub mod profile;
+    pub mod token;
+    pub mod auth;
+}
+pub mod resource {
+    pub mod model;
+    pub mod device;
+    pub mod group;
+    pub mod set;
+    pub mod data;
+    pub mod buffer;
+    pub mod slice;
+}
