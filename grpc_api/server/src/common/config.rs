ae6ab69996586e8b9501cc49b0a78e302469f695
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2025-12-04 09:14:25 +0700

    Create server config and utility functions

---
+++ grpc_api/server/src/common/config.rs
@@ -0,0 +1,93 @@
+use std::sync::OnceLock;
+use uuid::Uuid;
+use rsa::RsaPrivateKey;
+use crate::common::utility::{generate_transport_keys, export_public_key};
+use bbthings_database::{UserSchema, UserRoleSchema};
+use bbthings_database::utility::generate_access_key;
+
+pub const ROOT_ID: Uuid = Uuid::from_u128(0xffffffffffffffffffffffffffffffffu128);
+pub const ROOT_NAME: &str = "root";
+pub static ROOT_DATA: OnceLock<RootData> = OnceLock::new();
+
+pub static API_KEY: OnceLock<TransportKey> = OnceLock::new();
+pub static USER_KEY: OnceLock<TransportKey> = OnceLock::new();
+
+const DEF_ROOT_PW: &str = "r0ot_P4s5w0rd";
+const DEF_ACC_DUR: i32 = 300;
+const DEF_REF_DUR: i32 = 3600;
+const DEF_ACC_KEY: [u8; 32] = [0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255];
+
+#[derive(Debug, Clone)]
+pub struct RootData {
+    pub password: String,
+    pub access_duration: i32,
+    pub refresh_duration: i32,
+    pub access_key: Vec<u8>
+}
+
+impl Default for RootData {
+    fn default() -> Self {
+        Self {
+            password: String::from(DEF_ROOT_PW),
+            access_duration: DEF_ACC_DUR,
+            refresh_duration: DEF_REF_DUR,
+            access_key: DEF_ACC_KEY.to_vec()
+        }
+    }
+}
+
+impl RootData {
+    pub fn new(password: &str, access_duration: i32, refresh_duration: i32) -> Self {
+        Self {
+            password: String::from(password),
+            access_duration,
+            refresh_duration,
+            access_key: generate_access_key()
+        }
+    }
+    pub fn new_with_key(password: &str, access_duration: i32, refresh_duration: i32, root_key: &[u8]) -> Self {
+        Self {
+            password: String::from(password),
+            access_duration,
+            refresh_duration,
+            access_key: root_key.to_vec()
+        }
+    }
+}
+
+impl Into<UserSchema> for RootData {
+    fn into(self) -> UserSchema {
+        UserSchema {
+            id: ROOT_ID,
+            name: ROOT_NAME.to_owned(),
+            email: String::new(),
+            phone: String::new(),
+            password: String::from(self.password),
+            roles: vec![UserRoleSchema {
+                api_id: ROOT_ID,
+                role: ROOT_NAME.to_owned(),
+                multi: false,
+                ip_lock: true,
+                access_duration: self.access_duration,
+                refresh_duration: self.refresh_duration,
+                access_key: self.access_key.to_vec()
+            }]
+        }
+    }
+}
+
+pub struct TransportKey {
+    pub(crate) private_key: RsaPrivateKey,
+    pub(crate) public_der: Vec<u8>
+}
+
+impl TransportKey {
+    pub fn new() -> Self {
+        let (private_key, public_key) = generate_transport_keys().unwrap();
+        let public_der = export_public_key(public_key).unwrap();
+        Self {
+            private_key,
+            public_der
+        }
+    }
+}
---
+++ grpc_api/server/src/common/utility.rs
@@ -0,0 +1,71 @@
+use sha2::Sha256;
+use rsa::{RsaPrivateKey, RsaPublicKey, Oaep};
+use pkcs8::{DecodePublicKey, EncodePublicKey};
+use argon2::{Argon2, PasswordVerifier, password_hash::PasswordHash};
+use rand::thread_rng;
+
+pub fn generate_transport_keys() -> Result<(RsaPrivateKey, RsaPublicKey), rsa::Error>
+{
+    let mut rng = thread_rng();
+    let bits = 1024;
+    let priv_key = RsaPrivateKey::new(&mut rng, bits)?;
+    let pub_key = RsaPublicKey::from(&priv_key);
+    Ok((priv_key, pub_key))
+}
+
+pub fn export_public_key(pub_key: RsaPublicKey) -> Result<Vec<u8>, spki::Error>
+{
+    let pub_der = pub_key.to_public_key_der()?.to_vec();
+    Ok(pub_der)
+}
+
+pub fn import_public_key(pub_der: &[u8]) -> Result<RsaPublicKey, spki::Error>
+{
+    let pub_key = RsaPublicKey::from_public_key_der(pub_der)?;
+    Ok(pub_key)
+}
+
+pub fn decrypt_message(ciphertext: &[u8], priv_key: RsaPrivateKey) -> Result<Vec<u8>, rsa::Error>
+{
+    let padding = Oaep::new_with_mgf_hash::<Sha256, Sha256>();
+    priv_key.decrypt(padding, ciphertext)
+}
+
+pub fn encrypt_message(message: &[u8], pub_key: RsaPublicKey) -> Result<Vec<u8>, rsa::Error>
+{
+    let padding = Oaep::new_with_mgf_hash::<Sha256, Sha256>();
+    pub_key.encrypt(&mut thread_rng(), padding, message)
+}
+
+pub(crate) fn verify_password(password: &[u8], hash: &str) -> Result<(), argon2::password_hash::Error>
+{
+    let argon2 = Argon2::default();
+    let parsed_hash = PasswordHash::new(&hash)?;
+    argon2.verify_password(password, &parsed_hash)
+}
+
+pub(crate) fn handle_error(e: sqlx::Error) -> tonic::Status {
+    return match e {
+        sqlx::Error::RowNotFound => tonic::Status::not_found(e.to_string()),
+        sqlx::Error::InvalidArgument(message) => tonic::Status::invalid_argument(message),
+        sqlx::Error::Database(db_err) => {
+            match db_err.try_downcast::<sqlx::postgres::PgDatabaseError>() {
+                Ok(pg_err) => {
+                    let message = match pg_err.detail() {
+                        Some(detail) => format!("{} {}", pg_err.message(), detail),
+                        None => pg_err.message().to_string()
+                    };
+                    if pg_err.code() == "23505" {
+                        tonic::Status::already_exists(message)
+                    } else if pg_err.code() == "23501" || pg_err.code() == "23502" || pg_err.code() == "23503" {
+                        tonic::Status::invalid_argument(message)
+                    } else {
+                        tonic::Status::internal(message)
+                    }
+                },
+                Err(e) => tonic::Status::internal(e.message())
+            }
+        },
+        _ => tonic::Status::unknown(e.to_string())
+    }
+}
