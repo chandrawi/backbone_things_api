37545e62d5d9d88bdf716a6a9114d45e8281bfb7
Chandra Wijaya Sentosa <chandra.w.sentosa@gmail.com>
2025-12-09 17:01:42 +0700

    Create client_web project bundle

--- grpc_api/client_web/rollup.config.mjs
+++ grpc_api/client_web/rollup.config.mjs
@@ -2,19 +2,34 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import excludeDependenciesFromBundle from "rollup-plugin-exclude-dependencies-from-bundle";
 
-export default {
-    input: 'src/index.js',
-    output: {
-        file: 'index.js',
-        format: 'es'
+export default [
+    {
+        input: 'src/index.js',
+        output: {
+            file: 'bundle.js',
+            format: 'es'
+        },
+        plugins: [
+            resolve({
+                browser: true
+            }),
+            commonjs()
+        ]
     },
-    plugins: [
-        excludeDependenciesFromBundle({
-            dependencies: true
-        }),
-        resolve({
-            browser: true
-        }),
-        commonjs()
-    ]
-};
+    {
+        input: 'src/index.js',
+        output: {
+            file: 'index.js',
+            format: 'es'
+        },
+        plugins: [
+            excludeDependenciesFromBundle({
+                dependencies: true
+            }),
+            resolve({
+                browser: true
+            }),
+            commonjs()
+        ]
+    }
+];
